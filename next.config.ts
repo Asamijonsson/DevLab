f31caea6b49f90cca529a6e8551b34a153c405d7
Asami <90178926+Asamijonsson@users.noreply.github.com>
2025-06-13 13:42:49 +0200

    retry

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: "export", // Tells Next.js to use static export mode
+
+  images: {
+    unoptimized: true, // Required if you're using Next.js Image component
+  },
+};
 
 export default nextConfig;
