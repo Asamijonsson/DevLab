6d8397f5ee998f2f5a7bca0877460a83aab5316c
Asami <90178926+Asamijonsson@users.noreply.github.com>
2025-06-13 12:39:25 +0200

    created env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
-console.log("Firebase API Key:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
