3646eca666c78a65f39c0918f63c24ebf9322d2b
Asami <Asami@eden.local>
2025-10-06 10:51:53 +0200

    added key in deploy file

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -4,7 +4,7 @@ export default function Movies() {
   return (
     <div className=" p-14 bg-white flex flex-col items-center justify-center">
       <h1 className="pb-7 text-4xl text-black font-bold">
-        Popular Movies. (WIP)
+        Popular Movies (WIP)
       </h1>
       <AllMovies />
     </div>
